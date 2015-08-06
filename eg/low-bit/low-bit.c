a9c23db3b9ee83dd4e852c257816aaec723c4895
Stephen McCamant <mccamant@cs.umn.edu>
2015-08-06 00:35:34 -0500

    First example of FuzzBALL-based adaptor synthesis
    Currently with two simple arithmetic functions, a hand-written
    wrapper C program, and the CEGIS logic implemented in a Perl script
    to drive FuzzBALL. Right now pretty slow, the approach would also
    need changes before supporting a larger class of compared functions.

---
+++ eg/low-bit/low-bit.c
@@ -0,0 +1,162 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int f1(int x, unsigned y) {
+    return (x << 1) | (y % 2);
+}
+
+int f2(int a, int b, int c, int d) {
+    return a + b + (c & d);
+}
+
+/* Values assigned to this structure represent an adaptor. */
+struct adaptor {
+    int a_is_const; /* a_is_const = 1: "a" is a constant with value a_val */
+    int a_val;      /* a_is_const = 0: "a" is equal to the a_val'th arg */
+    int b_is_const;
+    int b_val;
+    int c_is_const;
+    int c_val;
+    int d_is_const;
+    int d_val;
+} the_adaptor;
+
+/* Adapted implementation of f1 using f2. Removing the assertions
+   would make this faster, but if we don't check somewhere that the
+   values are sensible, the search may generate non-sensible adaptors.
+   The adapter implementation and the assertions are written in a
+   weird way to show that they do not need any branches, though GCC
+   doesn't oblige in compiling them that way with the default settings
+   I used. */
+int f1_with_f2(struct adaptor *ap, int x, int y) {
+    int a, b, c, d;
+    assert(!(ap->a_is_const & ~1));
+    assert(ap->a_is_const | !(ap->a_val & ~1));
+    a = ap->a_is_const ? ap->a_val : (ap->a_val ? y : x);
+    assert(!(ap->b_is_const & ~1));
+    assert(ap->b_is_const | !(ap->b_val & ~1));
+    b = ap->b_is_const ? ap->b_val : (ap->b_val ? y : x);
+    assert(!(ap->c_is_const & ~1));
+    assert(ap->c_is_const | !(ap->c_val & ~1));
+    c = ap->c_is_const ? ap->c_val : (ap->c_val ? y : x);
+    assert(!(ap->d_is_const & ~1));
+    assert(ap->d_is_const | !(ap->d_val & ~1));
+    d = ap->d_is_const ? ap->d_val : (ap->d_val ? y : x);
+    return f2(a, b, c, d);
+}
+
+/* An adaptor can be represented in ASCII as a comma-seperated
+   sequence of fixed-width hex strings, as in:
+
+   0,00000000,0,00000001,1,0000face,1,ffffffff
+*/
+
+/* I wrote this parser without branches, but that feature is currently
+   not needed because we don't symbolically execute this code, we just
+   make its output structure symbolic. */
+#define is_hex(c) (((c) - '0' < 10u) | ((c) - 'a' < 6u))
+#define parse_hex(c) (((c) - '0' < 10u) ? (c) - '0' : (c) - 'a' + 10)
+
+void parse_adaptor(struct adaptor *ap, char *s) {
+    assert(strlen(s) == 43);
+    assert((s[0] & ~1) == '0');
+    ap->a_is_const = (s[0] & 1);
+    assert(s[1] == ',');
+    assert(is_hex(s[2]) & is_hex(s[3]) & is_hex(s[4]) & is_hex(s[5]) &
+	   is_hex(s[6]) & is_hex(s[7]) & is_hex(s[8]) & is_hex(s[9]));
+    ap->a_val = (parse_hex(s[2]) << 28) | (parse_hex(s[3]) << 24) |
+	        (parse_hex(s[4]) << 20) | (parse_hex(s[5]) << 16) |
+	        (parse_hex(s[6]) << 12) | (parse_hex(s[7]) <<  8) |
+         	(parse_hex(s[8]) <<  4) | (parse_hex(s[9]));
+    assert(s[10] == ',');
+
+    ap->b_is_const = (s[11] & 1);
+    assert(s[12] == ',');
+    assert(is_hex(s[13]) & is_hex(s[14]) & is_hex(s[15]) & is_hex(s[16]) &
+	   is_hex(s[17]) & is_hex(s[18]) & is_hex(s[19]) & is_hex(s[20]));
+    ap->b_val = (parse_hex(s[13]) << 28) | (parse_hex(s[14]) << 24) |
+	        (parse_hex(s[15]) << 20) | (parse_hex(s[16]) << 16) |
+	        (parse_hex(s[17]) << 12) | (parse_hex(s[18]) <<  8) |
+         	(parse_hex(s[19]) <<  4) | (parse_hex(s[20]));
+    assert(s[21] == ',');
+
+    ap->c_is_const = (s[22] & 1);
+    assert(s[23] == ',');
+    assert(is_hex(s[24]) & is_hex(s[25]) & is_hex(s[26]) & is_hex(s[27]) &
+	   is_hex(s[28]) & is_hex(s[29]) & is_hex(s[30]) & is_hex(s[31]));
+    ap->c_val = (parse_hex(s[24]) << 28) | (parse_hex(s[25]) << 24) |
+	        (parse_hex(s[26]) << 20) | (parse_hex(s[27]) << 16) |
+	        (parse_hex(s[28]) << 12) | (parse_hex(s[29]) <<  8) |
+         	(parse_hex(s[30]) <<  4) | (parse_hex(s[31]));
+    assert(s[32] == ',');
+
+    ap->d_is_const = (s[33] & 1);
+    assert(s[34] == ',');
+    assert(is_hex(s[35]) & is_hex(s[36]) & is_hex(s[37]) & is_hex(s[38]) &
+	   is_hex(s[39]) & is_hex(s[40]) & is_hex(s[41]) & is_hex(s[42]));
+    ap->d_val = (parse_hex(s[35]) << 28) | (parse_hex(s[36]) << 24) |
+	        (parse_hex(s[37]) << 20) | (parse_hex(s[38]) << 16) |
+	        (parse_hex(s[39]) << 12) | (parse_hex(s[40]) <<  8) |
+         	(parse_hex(s[41]) <<  4) | (parse_hex(s[42]));
+}
+
+/* The arguments to the printf()s are omitted for symbolic execution
+   purposes because concretizing the values to print them would cause
+   a lot of excess branching. */
+void compare(int x, int y) {
+    int r1, r2;
+    r1 = f1(x, y);
+    r2 = f1_with_f2(&the_adaptor, x, y);
+    if (r1 == r2) {
+	/* printf("Match, both give 0x%x\n", r1); */
+	printf("Match\n");
+    } else {
+	/*printf("Mismatch, f1 gives 0x%x, adaptor gives 0x%x\n", r1, r2);*/
+	printf("Mismatch\n");
+	exit(1);
+    }
+}
+
+int main(int argc, char **argv) {
+    int x, y;
+    char *env;
+    if (argc != 2 && argc != 3) {
+	fprintf(stderr, "Usage: low-bit input_file or low-bit x y\n");
+	return 10;
+    }
+    if ((env = getenv("ADAPTOR"))) {
+	parse_adaptor(&the_adaptor, env);
+    }
+    if (argc == 3) {
+	/* Compare the adapted and target implementations on one
+	   input, and print the results. If you explore this with x
+	   and y symbolic, you can check whether the adaptor works for
+	   all inputs. */
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	compare(x, y);
+    } else {
+	/* Try a sequence of tests from a file. This is the mode to
+	   use when you want the adaptor to be symbolic, to try to
+	   synthesize an adaptor that works over a whole test suite. */
+	FILE *fh;
+	if (argv[1][0] == '-' && argv[1][1] == 0) {
+	    fh = stdin;
+	} else {
+	    fh = fopen(argv[1], "r");
+	    if (!fh) {
+		fprintf(stderr, "Failed to open %s for reading: %s\n",
+			argv[1], strerror(errno));
+		return 1;
+	    }
+	}
+	while (fscanf(fh, "%d %d", &x, &y) != EOF) {
+	    compare(x, y);
+	}
+	printf("All tests succeeded!\n");
+    }
+    return 0;
+}
---
+++ eg/low-bit/synth.pl
@@ -0,0 +1,187 @@
+#!/usr/bin/perl
+
+use strict;
+
+# Only script argument is a random seed, to see new behavior
+srand($ARGV[0]);
+
+# Paths to binaries: these probably differ on your system
+my $fuzzball = "/home/smcc/bitblaze/fuzzball/trunk-gh/exec_utils/fuzzball";
+my $stp = "/home/smcc/bitblaze/fuzzball/trunk-gh/stp/stp";
+
+my $bin = "./low-bit";
+
+# Try to figure out the code and data addresses we need based on
+# matching the output of "nm" and "objdump". Not the most robust
+# possible approach.
+
+my $main_addr = "0x" . substr(`nm $bin | fgrep " T main"`, 0, 8);
+
+my($atoi_x_addr, $atoi_y_addr) =
+  map("0x0" . substr($_, 1, 7), `objdump -dr $bin | grep 'call.*atoi'`);
+
+my $fields_addr = "0x" . substr(`nm $bin | fgrep " B the_adaptor"`, 0, 8);
+
+# An adaptor is represented by a C struct, whose structure we
+# reproduce here.
+# Field [0]: offset within struct
+# Field [1]: field name
+# Field [2]: size in bytes (must be 1, 2, 4, or 8)
+# Field [3]: printf format for the string form
+# Field [4]: validity condition
+
+my @fields =
+  ([ 0, "a_is_const", 4, "%01x",
+        'a_is_const:reg32_t <= 1:reg32_t'],
+   [ 4, "a_val",      4, "%08x",
+        'a_is_const:reg32_t == 1:reg32_t | a_val:reg32_t <= 1:reg32_t'],
+   [ 8, "b_is_const", 4, "%01x",
+        'b_is_const:reg32_t <= 1:reg32_t'],
+   [12, "b_val",      4, "%08x",
+        'b_is_const:reg32_t == 1:reg32_t | b_val:reg32_t <= 1:reg32_t'],
+   [16, "c_is_const", 4, "%01x",
+        'c_is_const:reg32_t <= 1:reg32_t'],
+   [20, "c_val",      4, "%08x",
+        'c_is_const:reg32_t == 1:reg32_t | c_val:reg32_t <= 1:reg32_t'],
+   [24, "d_is_const", 4, "%01x",
+        'd_is_const:reg32_t <= 1:reg32_t'],
+   [28, "d_val",      4, "%08x",
+        'd_is_const:reg32_t == 1:reg32_t | d_val:reg32_t <= 1:reg32_t'],
+);
+
+my @solver_opts = ("-solver", "smtlib", "-solver-path", $stp);
+
+my @sym_field_opts;
+for my $fr (@fields) {
+    my $addr = sprintf("0x%08x", hex($fields_addr) + $fr->[0]);
+    my $name = $fr->[1];
+    my $size = {1 => "byte", 2 => "short",
+		4 => "word", 8 => "long"}->{$fr->[2]};
+    push @sym_field_opts, "-symbolic-$size", "$addr=$name";
+    if (@$fr > 4) {
+	push @sym_field_opts, "-extra-condition", $fr->[4];
+    }
+}
+#print join(" ", @sym_field_opts), "\n";
+
+# Given the specification of an adaptor, execute it with symbolic
+# inputs to either check it, or produce a counterexample.
+sub check_adaptor {
+    my($adapt) = @_;
+    my @args = ($fuzzball, "-linux-syscalls", $bin, "-env", "ADAPTOR=$adapt",
+		@solver_opts, "-fuzz-start-addr", $main_addr,
+		"-skip-call-ret-symbol-once", "$atoi_x_addr=x",
+		"-skip-call-ret-symbol-once", "$atoi_y_addr=y",
+		"-trace-iterations", "-trace-assigns", "-solve-final-pc",
+		"-random-seed", int(rand(10000000)),
+		"--", $bin, "0", "0");
+    open(LOG, "-|", @args);
+    my($matches, $fails) = (0, 0);
+    my(@ce, $this_ce);
+    $this_ce = 0;
+    while (<LOG>) {
+	if ($_ eq "Match\n") {
+	    $matches++;
+	} elsif ($_ eq "Mismatch\n") {
+	    $fails++;
+	    $this_ce = 1;
+	} elsif (/^Input vars: (.*)$/ and $this_ce) {
+	    my $vars = $1;
+	    @ce = (0, 0);
+	    for my $v (split(/ /, $vars)) {
+		if ($v =~ /^x=(0x[0-9a-f]+)$/) {
+		    $ce[0] = hex $1;
+		} elsif ($v =~ /^y=(0x[0-9a-f]+)$/) {
+		    $ce[1] = hex $1;
+		}
+	    }
+	    $this_ce = 0;
+	    print "  $_";
+	    last;
+	}
+	print "  $_";
+    }
+    close LOG;
+    if ($matches == 0 and $fails == 0) {
+	die "Missing results from check run";
+    }
+    if ($fails == 0) {
+	return 1;
+    } else {
+	return (0, [@ce]);
+    }
+}
+
+# Given a set of tests, run with the adaptor symbolic to see if we can
+# synthesize an adaptor that works for those tests.
+sub try_synth {
+    my($testsr) = @_;
+    open(TESTS, ">tests");
+    for my $t (@$testsr) {
+	print TESTS $t->[0], " ", $t->[1], "\n";
+    }
+    close TESTS;
+    my @args = ($fuzzball, "-linux-syscalls", $bin,
+		@solver_opts, "-fuzz-start-addr", $main_addr,
+		@sym_field_opts,
+		"-trace-iterations", "-trace-assigns", "-solve-final-pc",
+		"-random-seed", int(rand(10000000)),
+		"--", $bin, "tests");
+    open(LOG, "-|", @args);
+    my($success, %fields);
+    $success = 0;
+    while (<LOG>) {
+	if ($_ eq "All tests succeeded!\n") {
+	    $success = 1;
+	} elsif (/^Input vars: (.*)$/ and $success) {
+	    my $vars = $1;
+	    %fields = ();
+	    for my $v (split(/ /, $vars)) {
+		$v =~ /^(\w+)=(0x[0-9a-f]+)$/
+		  or die "Parse failure on variable assignment <$v>";
+		$fields{$1} = hex $2;
+	    }
+	    print "  $_";
+	    last;
+	}
+	print "  $_";
+    }
+    close LOG;
+    if (!$success) {
+	print "Synthesis failure: seems the functions are not equivalent.\n";
+	exit 2;
+    }
+    my @afields;
+    for my $fr (@fields) {
+	push @afields, sprintf($fr->[3], $fields{$fr->[1]});
+    }
+    return join(",", @afields);
+}
+
+
+# Main loop: starting with a stupid adaptor and no tests, alternate
+# between test generation and synthesis.
+my $adapt = "0,00000000,0,00000000,0,00000000,0,00000000";
+my @tests = ();
+my $done = 0;
+while (!$done) {
+    print "Checking $adapt:\n";
+    my($res, $cer) = check_adaptor($adapt);
+    if ($res) {
+	print "Success!\n";
+	print "Final test set:\n";
+	for my $tr (@tests) {
+	    print " $tr->[0], $tr->[1]\n";
+	}
+	print "Final adaptor is $adapt\n";
+	$done = 1;
+	last;
+    } else {
+	my($x, $y) = @$cer;
+	print "Mismatch on input $x, $y; adding as test\n";
+	push @tests, [$x, $y];
+    }
+
+    $adapt = try_synth(\@tests);
+    print "Synthesized adaptor $adapt\n";
+}
