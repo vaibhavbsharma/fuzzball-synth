2d52d313d754bdf7849b912d432859d3c6597b46
Vaibhav Sharma <vaibhav@umn.edu>
2016-04-08 19:03:20 -0500

    Adding utilities I am using to collect results from log files. These scripts assume results are placed in ../simple-1,../simple-2,...,../simple-8 since I am manually splitting the simple adaptor synthesis into 8 separate processes. The log filenames should be N-zero.txt or N-identity.txt where N is an integer between 1 and 8 and the zero/identity assume that the adaptor used to start the search process was the zero/identity adaptor. Both utilities can be given a -0/-1 option to make them look through the ../simple-N/N-zero.txt or ../simple-N/N-identity.txt log files respectively.

---
+++ eg/libc/simple/harvest_results.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+g_inequivalent=0;
+g_adaptors=0;
+g_missing=0;
+g_killed=0;
+g_total=0;
+g_strange_term=0;
+suffix="";
+echo -e '#\tIneq\t\t#adpt\tTimeout\tf1_crash\ttotal';
+if [ "$1" == "-0" ] ; then
+    suffix="-zero";
+else 
+    suffix="-identity";
+fi
+for i in {1..8}; do 
+    inequivalent=`grep 'not equivalent' ../simple-$i/$i$suffix.txt | wc -l`; 
+    adaptors=`grep 'Final adaptor' ../simple-$i/$i$suffix.txt | wc -l`; 
+    missing=`grep 'Missing results' ../simple-$i/$i$suffix.txt | wc -l`; 
+    killed=`grep 'killed by alarm' ../simple-$i/$i$suffix.txt | wc -l`;
+    strange_term=`grep 'Strange term' ../simple-$i/$i$suffix.txt | wc -l`;
+    total=$((inequivalent+adaptors+killed+missing));
+    echo -e $i '\t' $inequivalent '(' $strange_term ')\t' $adaptors '\t' $killed '\t' $missing '\t\t' $total; 
+    g_inequivalent=$((g_inequivalent+inequivalent));
+    g_adaptors=$((g_adaptors+adaptors));
+    g_missing=$((g_missing+missing));
+    g_killed=$((g_killed+killed));
+    g_total=$((g_total+total));
+    g_strange_term=$((g_strange_term+strange_term));
+done
+echo -e  'Total\t' $g_inequivalent '(' $g_strange_term ')\t' $g_adaptors '\t' $g_killed '\t' $g_missing '\t\t' $g_total; 
