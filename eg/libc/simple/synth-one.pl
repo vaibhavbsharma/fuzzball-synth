062b0afd9b9795edf12a55be4357929ccb15c625
Vaibhav Sharma <vaibhav@umn.edu>
2016-08-01 15:13:42 -0500

    Tracking symbolic region contents as part of counter-example for the simple argument substitution adaptor, similar to changes made for synth-typeconv.pl for this feature

--- eg/libc/simple/synth-one.pl
+++ eg/libc/simple/synth-one.pl
@@ -13,6 +13,10 @@ my $iteration_limit = 4000;
 
 my $region_limit = 936;
 
+my $sane_addr = 0x42420000;
+
+my @fuzzball_extra_args_arr;
+
 # Paths to binaries: these probably differ on your system. You can add
 # your locations to the list, or set the environment variable.
 my $smcc_umn = "/home/fac05/mccamant/bitblaze/fuzzball/trunk-gh";
@@ -64,7 +68,7 @@ close F;
 # matching the output of "nm" and "objdump". Not the most robust
 # possible approach.
 
-my $main_addr = "0x" . substr(`nm $bin | fgrep " T main"`, 0, 16);
+my $fuzz_start_addr = "0x" . substr(`nm $bin | fgrep " T fuzz_start"`, 0, 16);
 
 my $f1_addr = "0x" . substr(`nm $bin | fgrep " T f1"`, 0, 16);
 
@@ -92,7 +96,7 @@ my $match_jne_addr =
 
 print "$fuzzball\n";
 print "$stp\n";
-print "main: $main_addr\n";
+print "fuzz-start-addr : $fuzz_start_addr\n";
 print "f1:   $f1_addr @ $f1_call_addr\n";
 print "f2:   $f2_addr\n";
 print "wrap_f2: $wrap_f2_addr @ $f2_call_addr\n";
@@ -132,6 +136,8 @@ my @ret_fields =
 
 
 my($f1nargs, $f2nargs) = ($func_info[$f1num][1], $func_info[$f2num][1]);
+#$f1nargs=6;
+#$f2nargs=6;
 splice(@fields, 2 * $f2nargs);
 
 my @solver_opts = ("-solver", "smtlib-batch", "-solver-path", $stp, "-solver-timeout",5,"-timeout-as-unsat");
@@ -169,6 +175,7 @@ if($const_lb != $const_ub) {
 # inputs to either check it, or produce a counterexample.
 sub check_adaptor {
     my($adapt,$ret_adapt) = (@_);
+    #print "checking arg-adaptor = @$adapt ret-adaptor = @$ret_adapt\n";
     my @conc_adapt = ();
     for my $i (0 .. $#$adapt) {
 	my($name, $ty, $fmt) = @{$fields[$i]};
@@ -185,7 +192,7 @@ sub check_adaptor {
     }
     my @args = ($fuzzball, "-linux-syscalls", "-arch", "x64",
 		$bin,
-		@solver_opts, "-fuzz-start-addr", $main_addr,
+		@solver_opts, "-fuzz-start-addr", $fuzz_start_addr,
 		"-symbolic-long", "$arg_addr[0]=a",
 		"-symbolic-long", "$arg_addr[1]=b",
 		"-symbolic-long", "$arg_addr[2]=c",
@@ -204,7 +211,7 @@ sub check_adaptor {
 		#"-save-decision-tree-interval", 1,
 		#"-trace-decision-tree",
 		"-trace-binary-paths-bracketed",
-#"-narrow-bitwidth-cutoff","1",
+                #"-narrow-bitwidth-cutoff","1",
 		#"-trace-offset-limit",
 		"-trace-basic",
 		#"-trace-eip",
@@ -215,6 +222,7 @@ sub check_adaptor {
 		#"-trace-stores",
 		"-trace-conditions",
 		"-trace-decisions",
+		#"-trace-solver",
 		#"-save-solver-files", 
 		"-match-syscalls-in-addr-range",
 		$f1_call_addr.":".$post_f1_call.":".$f2_call_addr.":".$post_f2_call,
@@ -229,7 +237,6 @@ sub check_adaptor {
 		"-trace-stopping",
 		"-random-seed", int(rand(10000000)),
 		"--", $bin, $f1num, $f2num, "g");
-    #print "@args\n";
     my @printable;
     for my $a (@args) {
 	if ($a =~ /[\s|<>]/) {
@@ -241,7 +248,7 @@ sub check_adaptor {
     print "@printable\n";
     open(LOG, "-|", @args);
     my($matches, $fails) = (0, 0);
-    my(@ce, $this_ce);
+    my(@ce, $this_ce, @arg_to_regnum, @regnum_to_arg, @fuzzball_extra_args, @regnum_to_saneaddr);
     $this_ce = 0;
     my $f1_completed = 0;
     $f1_completed_count = 0;
@@ -251,11 +258,13 @@ sub check_adaptor {
 	    $matches++;
 	} elsif (/^Iteration (.*):$/) {
 	    $f1_completed = 0;
+	    @arg_to_regnum = (0) x $f1nargs;
+	    @regnum_to_arg = (0) x $f1nargs;
+	    @regnum_to_saneaddr = (0) x 256;
 	    $iteration_count++;
 	} elsif ($_ eq "Completed f1\n") {
 	    $f1_completed = 1;
 	    $f1_completed_count++;
-	    print "synth-one.pl: f1_completed = 1\n";
 	} elsif (($_ eq "Mismatch\n") or 
 		 (/^Stopping at null deref at (0x[0-9a-f]+)$/ and $f1_completed == 1) or
 		 (/^Stopping at access to unsupported address at (0x[0-9a-f]+)$/ and $f1_completed == 1) or
@@ -268,12 +277,40 @@ sub check_adaptor {
 	    @ce = (0) x $f1nargs;
 	    for my $v (split(/ /, $vars)) {
 		if ($v =~ /^([a-f])=(0x[0-9a-f]+)$/) {
-		    $ce[ord($1) - ord("a")] = hex $2;
+		    my $index = ord($1) - ord("a");
+		    $ce[$index] = hex $2;
+		    if ($arg_to_regnum[$index] != 0) {
+			$ce[$index] = $sane_addr;
+			$regnum_to_saneaddr[$arg_to_regnum[$index]] = $sane_addr;
+			$sane_addr = $sane_addr + $region_limit;
+		    }
+		} 
+	    }
+	    for my $v (split (/ /, $vars)) {
+		if($v =~ /^region_([0-9]+)_byte_0x([0-9]+)=(0x[0-9a-f]+)$/) {
+		    print "region assignment $1 $2 $3 for arg $regnum_to_arg[$1]\n";
+		    # push @fuzzball_extra_args, "-store-byte $regnum_to_saneaddr[$1]=$3";
+		    push @fuzzball_extra_args, "-store-byte";
+		    push @fuzzball_extra_args, sprintf("0x%x=%s",$regnum_to_saneaddr[$1]+$2,$3);
 		}
 	    }
+
 	    $this_ce = 0;
 	    print "  $_";
 	    last;
+	} elsif (/Address [a-f]_([0-9])+:reg64_t is region ([0-9]+)/ and $f1_completed == 0 ) {
+	    my $add_line = $_;
+	    my $add_var = -1;
+	    for my $v (split(/ /, $add_line)) {
+		if ($v =~ /^[a-f]_([0-9]+):reg64_t$/) { # matches argument name
+		    $add_var = ord($v) - ord('a');
+		} elsif ($v =~ /^[0-9]$/) { # matches region number
+		    if ($add_var < $f1nargs and $add_var >= 0) {
+			$arg_to_regnum[$add_var] = $v-'0';
+			$regnum_to_arg[$v-'0']=$add_var;
+		    }
+		}
+	    }
 	} 
 	print "  $_";
     }
@@ -284,14 +321,18 @@ sub check_adaptor {
     if ($fails == 0) {
 	return 1;
     } else {
-	return (0, [@ce]);
+	return (0, [@ce], [@fuzzball_extra_args]);
     }
 }
 
 # Given a set of tests, run with the adaptor symbolic to see if we can
 # synthesize an adaptor that works for those tests.
 sub try_synth {
-    my($testsr) = @_;
+    my($testsr, $_fuzzball_extra_args) = @_;
+    my @fuzzball_extra_args = @{ $_fuzzball_extra_args };
+    foreach my $i (0 .. $#fuzzball_extra_args) {
+	print "fuzzball_extra_args: fuzzball_extra_args[$i]= $fuzzball_extra_args[$i]\n";
+    }
     open(TESTS, ">tests");
     for my $t (@$testsr) {
 	my @vals = (@$t, (0) x 6);
@@ -302,8 +343,8 @@ sub try_synth {
     close TESTS;
     my @args = ($fuzzball, "-linux-syscalls", "-arch", "x64", $bin,
 		@solver_opts, 
-		"-fuzz-start-addr", $main_addr,
-		#"-trace-temps",
+		"-fuzz-start-addr", $fuzz_start_addr,
+		"-trace-temps",
 		#tell FuzzBALL to run in adaptor search mode, FuzzBALL will run in
 		#counter example search mode otherwise
 		"-adaptor-search-mode",
@@ -315,9 +356,29 @@ sub try_synth {
 		"-match-syscalls-in-addr-range",
 		$f1_call_addr.":".$post_f1_call.":".$f2_call_addr.":".$post_f2_call,
 		"-branch-preference", "$match_jne_addr:1",
-		#"-trace-conditions", "-trace-temps", "-omit-pf-af",
+		"-trace-conditions", "-omit-pf-af",
+		"-trace-syscalls",
+		#"-trace-decision-tree",
+		#"-save-decision-tree-interval","1",
+		"-trace-decisions",
 		"-trace-stopping",
+		"-trace-regions",
+		"-trace-binary-paths-bracketed",
 		"-trace-memory-snapshots",
+		"-trace-sym-addr-details",
+		"-trace-sym-addrs",
+		"-trace-tables",
+		#"-trace-offset-limit",
+		"-trace-basic",
+		#"-trace-eip",
+		#"-trace-registers",
+		#"-trace-stmts",
+		#"-trace-insns",
+		#"-trace-loads",
+		#"-trace-stores",
+		#"-trace-solver",
+		#"-save-solver-files", 
+		@fuzzball_extra_args,
 		"-region-limit", $region_limit,
 		"-random-seed", int(rand(10000000)),
 		"--", $bin, $f1num, $f2num, "f", "tests");
@@ -369,6 +430,13 @@ sub try_synth {
     return ([@afields],[@bfields]);
 }
 
+# Set these to test a specific adaptor
+#  $adapt->[0]=0;
+#  $adapt->[1]=0;
+#  $adapt->[2]=1;
+#  $adapt->[3]=0;
+#  $adapt->[4]=1;
+#  $adapt->[5]=10;
 
 # Main loop: starting with a stupid adaptor and no tests, alternate
 # between test generation and synthesis.
@@ -405,7 +473,7 @@ while (!$done) {
     my $adapt_s = join(",", @$adapt);
     my $ret_adapt_s = join(",", @$ret_adapt);
     print "Checking $adapt_s and $ret_adapt_s:\n";
-    my($res, $cer) = check_adaptor($adapt,$ret_adapt);
+    my($res, $cer, $_fuzzball_extra_args) = check_adaptor($adapt,$ret_adapt);
     if ($res) {
 	print "Success!\n";
 	print "Final test set:\n";
@@ -420,12 +488,13 @@ while (!$done) {
 	$done = 1;
 	last;
     } else {
+	push @fuzzball_extra_args_arr, @{ $_fuzzball_extra_args };
 	my $ce_s = join(", ", @$cer);
 	print "Mismatch on input $ce_s; adding as test\n";
 	push @tests, [@$cer];
     }
 
-    ($adapt,$ret_adapt) = try_synth(\@tests);
+    ($adapt,$ret_adapt) = try_synth(\@tests, \@fuzzball_extra_args_arr);
     print "Synthesized arg adaptor ".join(",",@$adapt).
 	" and return adaptor ".join(",",@$ret_adapt)."\n";
 }
