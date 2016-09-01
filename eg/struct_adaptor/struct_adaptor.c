6b5c171e81f732734c386969199148cbf4001f78
Vaibhav Sharma <vaibhav@umn.edu>
2016-09-01 15:22:24 -0500

    Using a different function pair that should is equivalent with a unique adaptor. The structure field substitution adaptor aka the memory layout permutation adaptor produces almost the right adaptor for this function pair (I think)

--- eg/struct_adaptor/struct_adaptor.c
+++ eg/struct_adaptor/struct_adaptor.c
@@ -81,7 +81,8 @@ int f1(struct1 *s) {
   if(s) {
     // s->a = 1;
     //return s->a - s->b;
-    return s->a - s->b - s->c + s->d;
+    //return s->a - s->b - s->c + s->d;
+    return 3*(s->a) + 5*(s->b) + 7*(s->c) + 11*(s->d);
     //return s->b;
   }
   return 0;
@@ -91,7 +92,8 @@ int f2(struct2 *s) {
   if(s) {
     // s->b = 1;
     //return s->b - s->a;
-    return s->b - s->a - s->c + s->d;
+    //return s->b - s->a - s->c + s->d;
+    return 3*(s->d) + 5*(s->c) + 7*(s->b) + 11*(s->a);
     //return s->a;
   }
   return 0;
