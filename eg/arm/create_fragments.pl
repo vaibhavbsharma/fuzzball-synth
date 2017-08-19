c012d8952b8a9ade7f0b094bd146e215794a0668
Vaibhav Sharma <vaibhav@umn.edu>
2017-08-19 10:11:46 -0500

    Create a separate .frag file for each return register

--- eg/arm/create_fragments.pl
+++ eg/arm/create_fragments.pl
@@ -3,9 +3,9 @@
 use strict;
 
 
-die "Usage: count-insns.pl <path-to-dump-file>"
-  unless @ARGV == 1;
-my ($dump_file) = @ARGV;
+die "Usage: count-insns.pl <path-to-dump-file> <fragment-prefix>"
+  unless @ARGV == 2;
+my ($dump_file,$file_prefix) = @ARGV;
 
 printf("dump_file = %s\n", $dump_file);
 my $line_num = 1;
@@ -14,13 +14,13 @@ my $start_num = 64842;
 my $end_num = 64846;
 
 
-open(FRAG, ">f");
-printf(FRAG "04 00 9b e5\n"); #   ldr     r0 [fp #4]\n");
-printf(FRAG "08 10 9b e5\n"); #   ldr     r1 [fp #8]\n");
-printf(FRAG "0c 20 9b e5\n"); #   ldr     r2 [fp #12]\n");
-printf(FRAG "10 30 9b e5\n"); #   ldr     r3 [fp #16]\n");
-printf(FRAG "14 40 9b e5\n"); #   ldr     r4 [fp #20]\n");
-printf(FRAG "18 50 9b e5\n"); #   ldr     r5 [fp #24]\n");
+my $frag_contents;
+$frag_contents .= sprintf("04 00 9b e5\n"); #   ldr     r0 [fp #4]\n");
+$frag_contents .= sprintf("08 10 9b e5\n"); #   ldr     r1 [fp #8]\n");
+$frag_contents .= sprintf("0c 20 9b e5\n"); #   ldr     r2 [fp #12]\n");
+$frag_contents .= sprintf("10 30 9b e5\n"); #   ldr     r3 [fp #16]\n");
+$frag_contents .= sprintf("14 40 9b e5\n"); #   ldr     r4 [fp #20]\n");
+$frag_contents .= sprintf("18 50 9b e5\n"); #   ldr     r5 [fp #24]\n");
 
 my @recent_w_regs = ("null", "null", "null");
 my $ind = 0;
@@ -48,62 +48,59 @@ while (<F>) {
 	my $b3 = substr $bytes, 4, 2;
 	my $b4 = substr $bytes, 6, 2;
 	printf(" $b4, $b3, $b2, $b1, // $insn_str\n");
-	printf(FRAG "$b4 $b3 $b2 $b1\n");
+	$frag_contents .= sprintf("$b4 $b3 $b2 $b1\n");
 	# print "$_";
     } 
     $line_num += 1;
 }
 printf("line_num = %d\n", $line_num);
+my $file_num = 1;
 my $ret_reg_byte = 0;
 for my $str (@recent_w_regs) {
+    my $added_ret_reg = 1;
+    my $ret_insn = "";
     if ($str =~ "r1") {
-	printf(FRAG "01 00 a0 e1\n"); #   mov    r0 r1\n"); 
+	$ret_insn = sprintf("01 00 a0 e1\n"); #   mov    r0 r1\n"); 
+    } elsif ($str =~ "r2") {
+	$ret_insn = sprintf("02 00 a0 e1\n"); #   mov    r0 r2\n"); 
+    } elsif ($str =~ "r3") {
+	$ret_insn = sprintf("03 00 a0 e1\n"); #   mov    r0 r3\n"); 
+    } elsif ($str =~ "r4") {
+	$ret_insn = sprintf("04 00 a0 e1\n"); #   mov    r0 r4\n"); 
+    } elsif ($str =~ "r5") {
+	$ret_insn = sprintf("05 00 a0 e1\n"); #   mov    r0 r5\n"); 
+    } elsif ($str =~ "r6") {
+	$ret_insn = sprintf("06 00 a0 e1\n"); #   mov    r0 r6\n"); 
+    } elsif ($str =~ "r7") {
+	$ret_insn = sprintf("07 00 a0 e1\n"); #   mov    r0 r7\n"); 
+    } elsif ($str =~ "r8") {
+	$ret_insn = sprintf("08 00 a0 e1\n"); #   mov    r0 r8\n"); 
+    } elsif ($str =~ "r9") {
+	$ret_insn = sprintf("09 00 a0 e1\n"); #   mov    r0 r9\n"); 
+    } elsif ($str =~ "sl") {
+	$ret_insn = sprintf("0a 00 a0 e1\n"); #   mov    r0 sl\n"); 
+    } elsif ($str =~ "fp") {
+	$ret_insn = sprintf("0b 00 a0 e1\n"); #   mov    r0 fp\n"); 
+    } elsif ($str =~ "ip") {
+	$ret_insn = sprintf("0c 00 a0 e1\n"); #   mov    r0 ip\n"); 
+    } elsif ($str =~ "sp") {
+	$ret_insn = sprintf("0d 00 a0 e1\n"); #   mov    r0 sp\n"); 
+    } elsif ($str =~ "lr") {
+	$ret_insn = sprintf("0e 00 a0 e1\n"); #   mov    r0 lr\n"); 
+    } else {
+	$ret_insn = sprintf("00 00 a0 e1\n"); #   nop\n");
+	$added_ret_reg = 0;
     }
-    elsif ($str =~ "r2") {
-	printf(FRAG "02 00 a0 e1\n"); #   mov    r0 r2\n"); 
-    }
-    elsif ($str =~ "r3") {
-	printf(FRAG "03 00 a0 e1\n"); #   mov    r0 r3\n"); 
-    }
-    elsif ($str =~ "r4") {
-	printf(FRAG "04 00 a0 e1\n"); #   mov    r0 r4\n"); 
-    }
-    elsif ($str =~ "r5") {
-	printf(FRAG "05 00 a0 e1\n"); #   mov    r0 r5\n"); 
-    }
-    elsif ($str =~ "r6") {
-	printf(FRAG "06 00 a0 e1\n"); #   mov    r0 r6\n"); 
-    }
-    elsif ($str =~ "r7") {
-	printf(FRAG "07 00 a0 e1\n"); #   mov    r0 r7\n"); 
-    }
-    elsif ($str =~ "r8") {
-	printf(FRAG "08 00 a0 e1\n"); #   mov    r0 r8\n"); 
-    }
-    elsif ($str =~ "r9") {
-	printf(FRAG "09 00 a0 e1\n"); #   mov    r0 r9\n"); 
-    }
-    elsif ($str =~ "sl") {
-	printf(FRAG "0a 00 a0 e1\n"); #   mov    r0 sl\n"); 
-    }
-    elsif ($str =~ "fp") {
-	printf(FRAG "0b 00 a0 e1\n"); #   mov    r0 fp\n"); 
-    }
-    elsif ($str =~ "ip") {
-	printf(FRAG "0c 00 a0 e1\n"); #   mov    r0 ip\n"); 
-    }
-    elsif ($str =~ "sp") {
-	printf(FRAG "0d 00 a0 e1\n"); #   mov    r0 sp\n"); 
-    }
-    elsif ($str =~ "lr") {
-	printf(FRAG "0e 00 a0 e1\n"); #   mov    r0 lr\n"); 
-    }
-    else {
+    if($added_ret_reg == 1) {
+	my $file_name = $file_prefix . "_" . $file_num . ".frag";
+	$file_num += 1;
+	open(FRAG, ">", $file_name);
+	printf(FRAG $frag_contents);
+	printf(FRAG $ret_insn);
+	printf(FRAG "1e ff 2f e1\n"); #   bx     lr\n");
 	printf(FRAG "00 00 a0 e1\n"); #   nop\n");
+	close FRAG;
     }
 }
-printf(FRAG "1e ff 2f e1\n"); #   bx     lr\n");
-printf(FRAG "00 00 a0 e1\n"); #   nop\n");
-close FRAG;
 
-printf("recent_w_regs = @recent_w_regs\n");
+# printf("recent_w_regs = @recent_w_regs\n");
