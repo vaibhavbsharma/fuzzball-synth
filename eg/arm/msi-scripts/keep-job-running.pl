c07fd909b46790910aae559eb1f29f3ad1217ff4
Vaibhav Sharma <vaibhav@umn.edu>
2017-09-25 18:43:40 -0500

    New perl script to keep a job running at MSI (Itasca)

---
+++ eg/arm/msi-scripts/keep-job-running.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+
+die "Usage: keep-job-running.pl <.qsub file name> <dir at itasca where .qsub file is located>"
+  unless @ARGV == 2;
+
+my ($job_name,$dir) = @ARGV;
+
+if( substr($job_name, length($job_name)-5) =~ ".qsub") {
+    $job_name = substr($job_name, 0, length($job_name)-5);
+}
+
+my $output = `ssh itasca "qstat -u vaibhav"`;
+my $found = 0;
+if(index($output, $job_name) != -1) { $found = 1; }
+printf("found = $found\n");
+if($found == 0) {
+    my $o = `ssh itasca "qsub $dir/$job_name.qsub"`;
+    printf("new job queued: output = $o\n");
+}
