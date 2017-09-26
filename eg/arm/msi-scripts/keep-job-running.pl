7a7a25fb454fac56a9581d6c9be309c395ed4389
Vaibhav Sharma <vaibhav@umn.edu>
2017-09-26 17:09:00 -0500

    Dont continue to run MSI job if you cannot ssh in

--- eg/arm/msi-scripts/keep-job-running.pl
+++ eg/arm/msi-scripts/keep-job-running.pl
@@ -12,6 +12,7 @@ if( substr($job_name, length($job_name)-5) =~ ".qsub") {
 }
 
 my $output = `ssh itasca "qstat -u vaibhav"`;
+if(index($output, "Connection closed") != -1) { printf("Connection closed!\n"); exit(1); }
 my $found = 0;
 if(index($output, $job_name) != -1) { $found = 1; }
 # printf("found = $found\n");
