0de10774a30c9a6ddcc58d3bb2881d242462a1a2
Vaibhav Sharma <vaibhav@umn.edu>
2017-09-29 08:26:24 -0500

    script to trim log files based on "identity-fragments = " string instances

---
+++ eg/arm/msi-scripts/trim-log.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+
+die "Usage: trim-log.pl <log file name> <number of \"identity-fragments\" to skip> <number of directories> <base dir. absolute path>"
+  unless @ARGV == 3;
+my($log_file_name,$skip_num, $num_dirs,$dir_path) = @ARGV;
+
+for(my $i=1; $i <= $num_dirs; $i++) {
+    my $count = 0;
+    my $trim_done = 0;
+    open(LOG,"<$dir_path/arm-$i/logs/$log_file_name");
+    open(TRIM_LOG,">$dir_path/arm-$i/logs/$log_file_name.trimmed");
+    while(<LOG>) {
+	if(/.*identity-fragments = .*/) { $count++; }
+	if($count == $skip_num) { $trim_done = 1; }
+	if($trim_done == 1) {
+	   print TRIM_LOG $_; 
+	}
+    }
+    close LOG;
+    close TRIM_LOG;
+}
