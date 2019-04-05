ae7542be59d7700eef98bbe745abfc3c7988aeba
Vaibhav Sharma <vaibhav@umn.edu>
2019-04-05 10:56:26 -0500

    minor changes to report-arith-adapters.sh that reports all the adapters found for a set of reference functions

--- eg/arm/msi-scripts/report-arith-adapters.sh
+++ eg/arm/msi-scripts/report-arith-adapters.sh
@@ -3,8 +3,8 @@
 if [ $# -lt 5 ]; then
 	echo "Your command line contains $# arguments";
 	echo "Usage: ./report-all-adapters.sh <dir. containing the ref<1-K> directories, e.g. /home/smccaman/vaibhav> <starting number for ref directories, e.g. 1> <ending number for ref directories, e.g. 25> <number of bucket dirs inside each ref dir, e.g. 32> <log file inside each ref-K/arm-N/logs/ dir., e.g. 13.log> [delete existing refK_arith_adapters.lst=1]";
-	echo "Sample usage: ./report-all-adapters.sh /home/smccaman/vaibhav 1 25 32 13.log";
-	echo "Sample usage: ./report-all-adapters.sh /home/smccaman/vaibhav 1 25 32 13.log 1 #deletes existing adapter lists";
+	echo "Sample usage: ./report-arith-adapters.sh /home/smccaman/vaibhav 1 25 32 13.log";
+	echo "Sample usage: ./report-arith-adapters.sh /home/smccaman/vaibhav 1 25 32 13.log 1 #deletes existing adapter lists";
 	exit 1;
 fi
 REFS_DIR=$1 # /home/smccaman/vaibhav
@@ -31,7 +31,7 @@ for ((i=$START_REF_NUM; i<=$END_REF_NUM; i++)); do
 	echo "generating $REFS_DIR/ref$i/ref$((i))_arith_adapters.lst";
 	for ((j=1; j<=$NUM_DIRS; j++)); do
 		# need 
-		grep "Final adaptor" $REFS_DIR/ref$i/arm-$j/logs/$LOG_FILE |& cut -d ' ' -f 7-87  >$REFS_DIR/ref$i/ref$((i))_arith_adapters.lst; 
+		grep "Final adaptor" $REFS_DIR/ref$i/arm-$j/logs/$LOG_FILE |& cut -d ' ' -f 7-87  >>$REFS_DIR/ref$i/ref$((i))_arith_adapters.lst; 
 	done;
 	echo "done";
 done
