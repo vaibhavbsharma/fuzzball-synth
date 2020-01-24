919b4e4e1e09960c675202510d0901028a48f060
Vaibhav Sharma <vaibhav@umn.edu>
2020-01-23 20:29:14 -0600

    adding call-insn-eip list to Palindrome repair

--- eg/repair/synth-argsub-repair-Palindrome.pl
+++ eg/repair/synth-argsub-repair-Palindrome.pl
@@ -9,6 +9,10 @@ my($arch, $rand_seed, $default_adapter_pref, $ret_adapter_on, $const_lb, $const_
 
 
 srand($rand_seed);
+
+my @target_frag_call_insn_opt = ( 
+		"-target-frag-call-insn-eips","0x804893a:0x80489bf:0x8048a34:0x8048ac3:0x806d4cc"); 
+# derived from WhackJack_pov_1.log by grepping for "target fragment call" see longer comment in synth-argsub-repair-TLR.pl
 my $input_len = 64;
 my $sym_prefix_suffix_size = 4; # $input_len/2; # makes the entire input be symbolic
 
@@ -466,6 +470,7 @@ sub try_synth {
     
     my @args = ($fuzzball, "-linux-syscalls", "-arch", $arch, $bin,
 		@solver_opts, 
+		@target_frag_call_insn_opt,
 		# "-dont-compare-linux-syscalls",
 		"-fuzz-start-addr", $fuzz_start_addr,
 		#tell FuzzBALL to run in adapter search mode, FuzzBALL will run in
